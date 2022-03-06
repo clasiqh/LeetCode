c53d91524cd32f80f0ce6121f9d09be17f20a306
clasiqh <shivam.chauhan126@gmail.com>
2022-03-06 14:23:13 +0530

    2190. Most Frequent Number Following Key In an Array

---
+++ Arrays/Most Frequent Number Following Key In an Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mostFrequent(int[] nums, int key) {
+        int[] arr = new int[1001];
+
+        for (int i = 0; i < nums.length - 1; i++)
+            if (nums[i] == key)
+                arr[nums[i + 1]] = arr[nums[i + 1]] + 1;
+
+        int max = 0;
+        int maxVal = -1;
+
+        for (int i = 1; i <= 1000; i++) {
+            if (maxVal < arr[i]) {
+                maxVal = arr[i];
+                max = i;
+            }
+        }
+        return max;
+    }
+}
