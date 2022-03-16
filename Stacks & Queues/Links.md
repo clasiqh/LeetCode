eea55bdb2fc57f62f0ffd62569ddef5cc3d317c3
clasiqh <shivam.chauhan126@gmail.com>
2022-03-16 19:22:20 +0530

    Validate Stack Sequences

--- Stacks & Queues/Links.md
+++ Stacks & Queues/Links.md
@@ -7,3 +7,7 @@ https://leetcode.com/problems/design-browser-history/
 ## 1249. Minimum Remove to Make Valid Parentheses
 
 https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+
+## 946. Validate Stack Sequences
+
+https://leetcode.com/problems/validate-stack-sequences/
---
+++ Stacks & Queues/Validate Stack Sequences.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int i = 0, j = 0;
+        for (int val : pushed) {
+            pushed[i++] = val;
+            while (j < popped.length && i > 0 && pushed[i - 1] == popped[j]) {
+                i--;
+                j++;
+            }
+        }
+        return i == 0;
+    }
+}
